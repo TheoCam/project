04242bce52108e583d3fe919c23da0803b645a62
TheoCam <94815818+TheoCam@users.noreply.github.com>
2022-05-07 13:54:15 +0800

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,15 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+using namespace std;
+
+
+void MoveCursorPosition(int x, int y);
+
+void PrintAtCursor(char ch, int x, int y);
+
+void create_game_area( );
+
+
+#endif
